e3f83b4c60140500774cf9abcb548c1b0475c0c1
moneshamurdeshwar <monsemurdeshwar@gmail.com>
2019-10-24 13:02:04 -0600

    Working on setting up controller input

---
+++ Assets/ViveGrab.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveGrab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // I was looking at this tutorial (https://circuitstream.com/blog/required-vive-code/) to figure out how to get the touchpad input, but it seems outdated.
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+
+    {
+
+        get
+
+        {
+
+            return SteamVR_Controller.Input((int)trackedObj.index);
+
+        }
+
+    }
+
+    void Awake()
+
+    {
+
+        trackedObj = GetComponent();
+
+    }
+}
