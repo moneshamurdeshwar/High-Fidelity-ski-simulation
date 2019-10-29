f29a6a22eb5925f22eeaa576cae65a1db432ce90
silverSapphire <31267159+silverSapphire@users.noreply.github.com>
2019-10-29 13:49:32 -0600

    Got trackpad input

--- Assets/ViveGrab.cs
+++ Assets/ViveGrab.cs
@@ -7,20 +7,23 @@ using Valve.VR.InteractionSystem;
 public class ViveGrab : MonoBehaviour
 {
 
-    private Hand hand;
-
     // Use this for initialization
     void Start()
     {
-        hand = gameObject.GetComponent<Hand>();
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(getTrackPadPos());
+
     }
 
     public Vector2 getTrackPadPos()
     {
         SteamVR_Action_Vector2 trackpadPos = SteamVR_Actions._default.touchpos;
 
-        Debug.Log(trackpadPos.GetAxis(hand.handType));
-        return trackpadPos.GetAxis(hand.handType);
+        return trackpadPos.GetAxis(SteamVR_Input_Sources.LeftHand);
     }
 
 }
