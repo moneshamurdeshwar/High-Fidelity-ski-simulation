4271127cb118b1a7af3e0bcfad999c69d9d98e81
silverSapphire <31267159+silverSapphire@users.noreply.github.com>
2019-10-30 19:43:12 -0600

    cleaned up comments

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,18 +6,11 @@ using Valve.VR;
 
 public class Movement : MonoBehaviour
 {
-    private Vector3 movementVector;
-    private GameObject characterController;
-    private int speed = 1;
+    private int speed = 5;
 
     void Start()
     {
-        characterController = GameObject.FindGameObjectWithTag("Player");
-    }
 
-    private void Move()
-    {
-        characterController.transform.position = movementVector * Time.deltaTime;
     }
 
     void Update()
@@ -26,19 +19,7 @@ public class Movement : MonoBehaviour
         SteamVR_Action_Boolean forward = SteamVR_Actions._default.forward;
 
         if (forward.GetLastState(SteamVR_Input_Sources.LeftHand))
-            transform.position = transform.position + Camera.main.transform.forward * 10 * Time.deltaTime;
-
-        //if (forward.GetLastState(SteamVR_Input_Sources.LeftHand))
-        //    //movementVector.x *= speed;
-        //    characterController.transform.position = new Vector3(characterController.transform.position.x + 1, characterController.transform.position.y, characterController.transform.position.z);
-
-        //Move();
-
-        //movementVector.x = Input.GetAxis("LeftJoystickX") * -speed;
-        //movementVector.z = Input.GetAxis("LeftJoystickY") * speed;
-
-        //movementVector.y = 0;
-        //characterController.Move(movementVector * Time.deltaTime);
+            transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
 
     }
 }
