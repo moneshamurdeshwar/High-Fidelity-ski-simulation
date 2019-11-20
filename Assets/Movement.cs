f72afb27900504aa3e35f124e546de26c5e6c819
silverSapphire <31267159+silverSapphire@users.noreply.github.com>
2019-11-20 14:20:50 -0700

    got tracker tracking but no movement effect yet

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -11,23 +11,46 @@ public class Movement : MonoBehaviour
     private Vector3 movement;
     private bool move;
 
+    public bool usingTracker = false;
+
+    private Transform tracker;
+    private Transform sitSki; 
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        tracker = transform.Find("Tracker");
+        sitSki = transform.Find("SitSki");
+
+        if (!sitSki)
+            Debug.Log("CAN'T FIND SITSKI");
     }
 
     void Update()
     {
 
-        SteamVR_Action_Boolean forward = SteamVR_Actions._default.forward;
-        move = forward.GetLastState(SteamVR_Input_Sources.LeftHand) || forward.GetLastState(SteamVR_Input_Sources.RightHand);
-        movement = Camera.main.transform.forward;
+        if(!usingTracker)
+        {
+            SteamVR_Action_Boolean forward = SteamVR_Actions._default.forward;
+            move = forward.GetLastState(SteamVR_Input_Sources.LeftHand) || forward.GetLastState(SteamVR_Input_Sources.RightHand);
+            movement = Camera.main.transform.forward;
 
-        if (forward.GetLastState(SteamVR_Input_Sources.LeftHand) || forward.GetLastState(SteamVR_Input_Sources.RightHand))
-            //    //transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
-            //rb.MovePosition(transform.position + Camera.main.transform.forward * speed * Time.deltaTime);
-            rb.AddForce(Camera.main.transform.forward * speed);
+            sitSki.rotation = Quaternion.FromToRotation(sitSki.rotation.eulerAngles, Camera.main.transform.forward);
+            //Vector3 rotation = sitSki.eulerAngles;
+            //rotation.z += 180;
+            //sitSki.rotation = Quaternion.Euler(rotation);
 
+            if (forward.GetLastState(SteamVR_Input_Sources.LeftHand) || forward.GetLastState(SteamVR_Input_Sources.RightHand))
+                //    //transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+                //rb.MovePosition(transform.position + Camera.main.transform.forward * speed * Time.deltaTime);
+                rb.AddForce(Camera.main.transform.forward * speed);
+        }
+
+        else
+        {
+            Debug.Log(tracker.eulerAngles.x);
+        }
+        
     }
 
     //private void FixedUpdate()
